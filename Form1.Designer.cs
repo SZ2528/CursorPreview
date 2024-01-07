370c04a45c173288d2f667fe198247089712db6d
SZ2528 <83620631+SZ2528@users.noreply.github.com>
2024-01-07 21:21:57 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace CursorPreview;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        label1 = new Label();
+        label2 = new Label();
+        label3 = new Label();
+        label4 = new Label();
+        label5 = new Label();
+        label6 = new Label();
+        label7 = new Label();
+        label8 = new Label();
+        label9 = new Label();
+        label10 = new Label();
+        label11 = new Label();
+        label12 = new Label();
+        label13 = new Label();
+        label14 = new Label();
+        label15 = new Label();
+        label16 = new Label();
+        label17 = new Label();
+        SuspendLayout();
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Location = new Point(32, 32);
+        label1.Name = "label1";
+        label1.Size = new Size(110, 31);
+        label1.TabIndex = 0;
+        label1.Text = "正常选择";
+        // 
+        // label2
+        // 
+        label2.AutoSize = true;
+        label2.Cursor = Cursors.Help;
+        label2.Location = new Point(196, 32);
+        label2.Name = "label2";
+        label2.Size = new Size(110, 31);
+        label2.TabIndex = 1;
+        label2.Text = "帮助选择";
+        // 
+        // label3
+        // 
+        label3.AutoSize = true;
+        label3.Cursor = Cursors.AppStarting;
+        label3.Location = new Point(383, 32);
+        label3.Name = "label3";
+        label3.Size = new Size(110, 31);
+        label3.TabIndex = 2;
+        label3.Text = "后台运行";
+        // 
+        // label4
+        // 
+        label4.AutoSize = true;
+        label4.Cursor = Cursors.WaitCursor;
+        label4.Location = new Point(640, 32);
+        label4.Name = "label4";
+        label4.Size = new Size(38, 31);
+        label4.TabIndex = 3;
+        label4.Text = "忙";
+        // 
+        // label5
+        // 
+        label5.AutoSize = true;
+        label5.Cursor = Cursors.Cross;
+        label5.Location = new Point(32, 95);
+        label5.Name = "label5";
+        label5.Size = new Size(110, 31);
+        label5.TabIndex = 4;
+        label5.Text = "精确选择";
+        // 
+        // label6
+        // 
+        label6.AutoSize = true;
+        label6.Cursor = Cursors.IBeam;
+        label6.Location = new Point(196, 95);
+        label6.Name = "label6";
+        label6.Size = new Size(110, 31);
+        label6.TabIndex = 5;
+        label6.Text = "文本选择";
+        // 
+        // label7
+        // 
+        label7.AutoSize = true;
+        label7.Location = new Point(383, 95);
+        label7.Name = "label7";
+        label7.Size = new Size(82, 31);
+        label7.TabIndex = 6;
+        label7.Text = "label7";
+        // 
+        // label8
+        // 
+        label8.AutoSize = true;
+        label8.Cursor = Cursors.No;
+        label8.Location = new Point(640, 95);
+        label8.Name = "label8";
+        label8.Size = new Size(86, 31);
+        label8.TabIndex = 7;
+        label8.Text = "不可用";
+        // 
+        // label9
+        // 
+        label9.AutoSize = true;
+        label9.Cursor = Cursors.SizeNS;
+        label9.Location = new Point(32, 168);
+        label9.Name = "label9";
+        label9.Size = new Size(158, 31);
+        label9.TabIndex = 8;
+        label9.Text = "垂直调整大小";
+        // 
+        // label10
+        // 
+        label10.AutoSize = true;
+        label10.Cursor = Cursors.SizeWE;
+        label10.Location = new Point(196, 168);
+        label10.Name = "label10";
+        label10.Size = new Size(158, 31);
+        label10.TabIndex = 9;
+        label10.Text = "水平调整大小";
+        // 
+        // label11
+        // 
+        label11.AutoSize = true;
+        label11.Cursor = Cursors.SizeNWSE;
+        label11.Location = new Point(383, 168);
+        label11.Name = "label11";
+        label11.Size = new Size(227, 31);
+        label11.TabIndex = 10;
+        label11.Text = "沿对角线调整大小 1";
+        // 
+        // label12
+        // 
+        label12.AutoSize = true;
+        label12.Cursor = Cursors.SizeNESW;
+        label12.Location = new Point(640, 168);
+        label12.Name = "label12";
+        label12.Size = new Size(227, 31);
+        label12.TabIndex = 11;
+        label12.Text = "沿对角线调整大小 1";
+        // 
+        // label13
+        // 
+        label13.AutoSize = true;
+        label13.Cursor = Cursors.SizeAll;
+        label13.Location = new Point(32, 237);
+        label13.Name = "label13";
+        label13.Size = new Size(62, 31);
+        label13.TabIndex = 12;
+        label13.Text = "移动";
+        // 
+        // label14
+        // 
+        label14.AutoSize = true;
+        label14.Cursor = Cursors.UpArrow;
+        label14.Location = new Point(196, 237);
+        label14.Name = "label14";
+        label14.Size = new Size(62, 31);
+        label14.TabIndex = 13;
+        label14.Text = "候选";
+        // 
+        // label15
+        // 
+        label15.AutoSize = true;
+        label15.Location = new Point(383, 237);
+        label15.Name = "label15";
+        label15.Size = new Size(96, 31);
+        label15.TabIndex = 14;
+        label15.Text = "label15";
+        // 
+        // label16
+        // 
+        label16.AutoSize = true;
+        label16.Location = new Point(640, 237);
+        label16.Name = "label16";
+        label16.Size = new Size(96, 31);
+        label16.TabIndex = 15;
+        label16.Text = "label16";
+        // 
+        // label17
+        // 
+        label17.AutoSize = true;
+        label17.Location = new Point(32, 308);
+        label17.Name = "label17";
+        label17.Size = new Size(96, 31);
+        label17.TabIndex = 16;
+        label17.Text = "label17";
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(14F, 31F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(1000, 400);
+        Controls.Add(label17);
+        Controls.Add(label16);
+        Controls.Add(label15);
+        Controls.Add(label14);
+        Controls.Add(label13);
+        Controls.Add(label12);
+        Controls.Add(label11);
+        Controls.Add(label10);
+        Controls.Add(label9);
+        Controls.Add(label8);
+        Controls.Add(label7);
+        Controls.Add(label6);
+        Controls.Add(label5);
+        Controls.Add(label4);
+        Controls.Add(label3);
+        Controls.Add(label2);
+        Controls.Add(label1);
+        Name = "Form1";
+        Text = "CursorPreview";
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private Label label1;
+    private Label label2;
+    private Label label3;
+    private Label label4;
+    private Label label5;
+    private Label label6;
+    private Label label7;
+    private Label label8;
+    private Label label9;
+    private Label label10;
+    private Label label11;
+    private Label label12;
+    private Label label13;
+    private Label label14;
+    private Label label15;
+    private Label label16;
+    private Label label17;
+}
---
+++ Form1.cs
@@ -0,0 +1,9 @@
+namespace CursorPreview;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
